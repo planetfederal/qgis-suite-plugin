a2a07fd30a5897053bac44953c8576e216f1a30b
Victor Olaya <volayaf@gmail.com>
2013-08-25 11:34:15 +0200

    updated version number

--- src/opengeo/__init__.py
+++ src/opengeo/__init__.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
-
-#===============================================================================
-# from sextante.tools.general import runalg, runandload, alghelp, alglist, algoptions, load, extent, getobject
-# from sextante.tools.vector import getfeatures, spatialindex, values, uniquevalues
-# from sextante.tools.raster import scanraster
-# from sextante.tests.TestData import loadTestData
-#===============================================================================
-
 from opengeo.qgis.catalog import *
 
 def classFactory(iface):

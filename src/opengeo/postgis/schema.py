36fa9ca5ba714ce3499aa92fe3b861a642ec31ae
Victor Olaya <volayaf@gmail.com>
2013-11-15 08:47:43 +0100

    do not show raster metatables in PG

--- src/opengeo/postgis/schema.py
+++ src/opengeo/postgis/schema.py
@@ -8,7 +8,7 @@ class Schema(object):
         
     def tables(self):
         tables = self.conn.geodb.list_geotables(self.name) 
-        blacklist = ['geometry_columns', 'geography_columns', 'spatial_ref_sys']
+        blacklist = ['geometry_columns', 'geography_columns', 'spatial_ref_sys', 'raster_columns', 'raster_overviews']
         return [Table(self.conn, self.name, table[0], table[2], table[6], table[7], table[9]) 
                 for table in tables if table[0] not in blacklist and table[6] is not None]
     

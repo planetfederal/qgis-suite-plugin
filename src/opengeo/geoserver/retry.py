40d66802ea1905c503843f83fd59ae1531c3b727
volaya <volayaf@gmail.com>
2015-03-30 09:13:39 +0200

    New implementation of RetryCatalog.
    Instead of decorating the catalog, it decorates the underlying http connection

--- src/opengeo/geoserver/retry.py
+++ src/opengeo/geoserver/retry.py
@@ -1,8 +1,9 @@
 from functools import wraps
 from geoserver.catalog import Catalog
+import httplib2 
+from urlparse import urlparse
 
 def retryMethodDecorator(func):
-    @wraps(func)
     def decorator(*args, **kwargs):
         try:
             result = func(*args, **kwargs)
@@ -15,8 +16,27 @@ def retryMethodDecorator(func):
     return decorator
 
 class RetryCatalog(Catalog):
+
+    def setup_connection(self):
+        self.http = RetryConnection(
+            disable_ssl_certificate_validation=self.diable_ssl_cert_validation)
+        self.http.add_credentials(self.username, self.password)
+        netloc = urlparse(self.service_url).netloc
+        self.http.authorizations.append(
+            httplib2.BasicAuthentication(
+                (self.username, self.password),
+                netloc,
+                self.service_url,
+                {},
+                None,
+                None,
+                self.http
+            ))
+
+
+class RetryConnection(httplib2.Http):
     def __getattribute__(self, attr_name):
-        obj = super(RetryCatalog, self).__getattribute__(attr_name)
+        obj = super(httplib2.Http, self).__getattribute__(attr_name)
         if hasattr(obj, '__call__') and hasattr(obj, '__name__'):
             if not obj.__name__.startswith('__'):
                 return retryMethodDecorator(obj)

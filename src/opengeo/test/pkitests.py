a597b927cb9670a84817d84c37dc50ed7a80b96e
Larry Shaffer <larrys@dakotacarto.com>
2015-05-27 08:02:43 -0600

    Update PKI code to match core QGIS changes, with backward compatibility

--- src/opengeo/test/pkitests.py
+++ src/opengeo/test/pkitests.py
@@ -8,6 +8,8 @@ keyfile = os.path.join(os.path.dirname(__file__), "resources", "rod.key.pem")
 certfile = os.path.join(os.path.dirname(__file__), "resources", "rod.crt.pem")
 cafile =  os.path.join(os.path.dirname(__file__), "resources", "ca.pem")
 
+# FIXME: the params for a PKI URL have changed: should only be 'authcfg=(7 char alpha-numeric)'
+#        the auth config associated with it will need to pre-exist in auth database
 class PKITests(unittest.TestCase):
     '''
     Tests for PKI support in QGIS

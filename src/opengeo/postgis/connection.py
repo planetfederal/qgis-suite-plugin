deee950745f831e5d1fc1e4ab43e31908c433ce5
volaya <volayaf@gmail.com>
2015-06-09 07:05:47 +0200

    reverted to postgis import mechanism using QGIS classes instead of Processing
    The processing algorithm uses ogr2ogr, and it was failin gor not importing all data in certain cases

--- src/opengeo/postgis/connection.py
+++ src/opengeo/postgis/connection.py
@@ -2,9 +2,9 @@ from postgis_utils import GeoDB
 from schema import Schema
 from qgis.core import *
 from PyQt4 import QtCore
-from opengeo.gui.dialogs.gsnamedialog import getPostGisTableName
-from opengeo.gui.gsnameutils import xmlNameIsValid, xmlNameRegex
-
+import re
+import subprocess
+import os
 
 class PgConnection(object):
 
@@ -36,55 +36,94 @@ class PgConnection(object):
 
     def importFileOrLayer(self, source, schema, tablename, overwrite, singleGeom = False):
 
-        if isinstance(source, basestring):
-            layerName = QtCore.QFileInfo(source).completeBaseName()
+        if overwrite:
+            pk = "id"
+            geom = "geom"
+            providerName = "postgres"
+
+            if isinstance(source, basestring):
+                layerName = QtCore.QFileInfo(source).completeBaseName()
+            else:
+                layerName = source.name()
+
+            if tablename is None:
+                tablename = layerName
+
+            uri = QgsDataSourceURI()
+            uri.setConnection(self.geodb.host, str(self.geodb.port), self.geodb.dbname, self.geodb.user, self.geodb.passwd)
+            uri.setDataSource(schema, tablename, geom, "", pk)
+
+            options = {}
+            options['overwrite'] = True
+            if singleGeom:
+                options['forceSinglePartGeometryType'] = True
+
+            if isinstance(source, basestring):
+                layer = QgsVectorLayer(source, layerName, "ogr")
+                if not layer.isValid() or layer.type() != QgsMapLayer.VectorLayer:
+                    layer.deleteLater()
+                    raise WrongLayerFileError("Error reading file {} or it is not a valid vector layer file".format(source))
+            else:
+                layer = source
+                if not layer.isValid() or layer.type() != QgsMapLayer.VectorLayer:
+                    raise WrongLayerFileError("Layer '%s' is not valid or is not a vector layer".format(layer.name()))
+
+            ret, errMsg = QgsVectorLayerImport.importLayer(layer, uri.uri(), providerName, layer.crs(), False, False, options)
+            if ret != 0:
+                raise Exception(errMsg)
         else:
-            layerName = source.name()
+            if isinstance(source, QgsMapLayer):
+                source = source.source()
 
-        if tablename is None:
-            tablename = layerName
+            args = ["shp2pgsql", "-a"]
+            if singleGeom:
+                args.append("-S")
+            args.extend([source, schema + "." + tablename])
+            if os.name == 'nt':
+                cmdline = subprocess.list2cmdline(args)
+                data = None
 
-        if not xmlNameIsValid(tablename, xmlNameRegex()):
-            tablename = getPostGisTableName(name=tablename, names=[], unique=False)
+                p = os.popen3(cmdline)
+                data = p[1].read()
 
-        if isinstance(source, basestring):
-            layer = QgsVectorLayer(source, layerName, "ogr")
-            if not layer.isValid() or layer.type() != QgsMapLayer.VectorLayer:
-                layer.deleteLater()
-                raise WrongLayerFileError("Error reading file {} or it is not a valid vector layer file".format(source))
-        else:
-            layer = source
-            if not layer.isValid() or layer.type() != QgsMapLayer.VectorLayer:
-                raise WrongLayerFileError("Layer '%s' is not valid or is not a vector layer".format(layer.name()))
+                cursor = self.geodb.con.cursor()
+                newcommand = re.compile(";$", re.MULTILINE)
 
-        extent = '{},{},{},{}'.format(
-                layer.extent().xMinimum(), layer.extent().xMaximum(),
-                layer.extent().yMinimum(), layer.extent().yMaximum())
-        geomtypes = {QGis.WKBPoint: 3,
-                     QGis.WKBLineString: 4,
-                     QGis.WKBPolygon: 5,
-                     QGis.WKBMultiPoint: 7,
-                     QGis.WKBMultiLineString: 9,
-                     QGis.WKBMultiPolygon: 8}
-        geomtype = geomtypes.get(layer.wkbType(), 0)
-        import processing
-        from processing.algs.gdal.ogr2ogrtopostgis import Ogr2OgrToPostGis as ogr2ogr
-        params = {ogr2ogr.INPUT_LAYER: layer,
-                    ogr2ogr.DBNAME: self.geodb.dbname,
-                    ogr2ogr.PORT : self.geodb.port,
-                    ogr2ogr.HOST : self.geodb.host,
-                    ogr2ogr.USER : self.geodb.user,
-                    ogr2ogr.PASSWORD: self.geodb.passwd,
-                    ogr2ogr.SCHEMA: schema,
-                    ogr2ogr.GTYPE: geomtype,
-                    ogr2ogr.TABLE: tablename,
-                    ogr2ogr.S_SRS: layer.crs().authid(),
-                    ogr2ogr.T_SRS: layer.crs().authid(),
-                    ogr2ogr.OVERWRITE: overwrite,
-                    ogr2ogr.APPEND: not overwrite,
-                    ogr2ogr.SPAT: extent
-                    }
-        processing.runalg("gdalogr:importvectorintopostgisdatabasenewconnection", params)
+                # split the commands
+                cmds = newcommand.split(data)
+                for cmd in cmds[:-1]:
+                    # run SQL commands within current DB connection
+                    self.geodb._exec_sql(cursor, cmd)
+                data = cmds[-1]
+
+                self.geodb.con.commit()
+
+                if data is None or len(data) == 0:
+                    raise Exception("".join(p[2].readlines()))
+            else:
+                # start shp2pgsql as subprocess
+                p = subprocess.Popen(args=args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                # read the output while the process is running
+                data = ''
+                cursor = self.geodb.con.cursor()
+                newcommand = re.compile(";$", re.MULTILINE)
+                while p.poll() == None:
+                    data += p.stdout.read()
+
+                    # split the commands
+                    cmds = newcommand.split(data)
+                    for cmd in cmds[:-1]:
+                        # run SQL commands within current DB connection
+                        self.geodb._exec_sql(cursor, cmd)
+                    data = cmds[-1]
+
+                # commit!
+                self.db.con.commit()
+
+                if p.returncode != 0:
+                    raise Exception(p.stderr.readlines().join("\n"))
+
 
 class WrongLayerFileError(Exception):
     pass
